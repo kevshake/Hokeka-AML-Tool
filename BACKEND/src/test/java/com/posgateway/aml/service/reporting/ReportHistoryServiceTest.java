43c05bdf5891e1f28e60018bdc7aee9f468529ca
fayywamwangi-eng <fayy.wa.mwangi@gmail.com>
2026-05-04 13:06:43 +0300

    Update ReportHistoryServiceTest.java

--- BACKEND/src/test/java/com/posgateway/aml/service/reporting/ReportHistoryServiceTest.java
+++ BACKEND/src/test/java/com/posgateway/aml/service/reporting/ReportHistoryServiceTest.java
@@ -29,7 +29,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReportHistoryServiceTest {
