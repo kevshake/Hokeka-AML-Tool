2214d5a958e361136318c6ce0610337bbc31c16b
Kevin <kevshakes@hotmail.com>
2026-05-21 12:22:55 +0300

    chore: OpenAPI version bump and continued integration

--- BACKEND/src/main/java/com/posgateway/aml/config/OpenApiConfig.java
+++ BACKEND/src/main/java/com/posgateway/aml/config/OpenApiConfig.java
@@ -34,7 +34,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Hokeka AML Platform API")
-                        .version("1.8.0")
+                        .version("1.9.0")
                         .description("""
                                 Anti-Money Laundering and Fraud Detection System API
                                 
